ac32edd9117f3363346d51d3510228a6c855c22d
Bruno-VILLALON <brunovillalon@gmail.com>
2022-12-05 16:30:18 +0100

    add netlify-form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,9 +4,16 @@ const Contact = () => {
     return (
         <div className='contact' id="contact">
             <h2>CONTACTE NOUS</h2>
-            <form>
-                <input type="text" placeholder="Prénom, Nom" required />
-                <input type="email" placeholder="E-mail" required />
+            <form
+                name='contact'
+                method='post'
+                className='contact__form'
+                data-netlify='true'
+                onSubmit='submit'
+                >
+                <input type='hidden' name='form-contact' value='contact' />
+                <input type="text" name='name' placeholder="Prénom, Nom" required />
+                <input type="email" name='email' placeholder="E-mail" required />
                 <textarea name="message" placeholder="Ton message ......"></textarea>
                 <input type="submit" value="Envoyer" />
             </form>
